b68ba022a0ef90243d4779cb69de740a315c98d3
jamesturbett <jturbett@outlook.com>
2025-10-02 18:18:51 +0100

    Refactor calculate_pamm_bounds function: rename parameters and improve variable naming for clarity

--- python/pamm.py
+++ python/pamm.py
@@ -1,27 +1,27 @@
-def calculate_pamm_bounds(
-    progs: int, prefs_before: int, ratio_after_ukfpo: float
+def calc_pamm_bounds(
+    progs: int, prefs_before: int, pub_ratio_after: float
 ) -> int | tuple[int, int]:
 
-    max_possible_pamm = min(progs, prefs_before)
+    max_poss_pamm = min(progs, prefs_before)
 
-    valid_pamm: list[int] = []
+    valid_pamm_vals: list[int] = []
 
-    # Try each possible PAMM value, and see if it yields the published competition ratio after PAMM
-    for pamm in range(max_possible_pamm + 1):
-        prefs_after = prefs_before - pamm
-        progs_after = progs - pamm
+    # Try each possible num_pamm value, and see if it yields the pub competition ratio after num_pamm
+    for num_pamm in range(max_poss_pamm + 1):
+        prefs_after = prefs_before - num_pamm
+        progs_after = progs - num_pamm
         if (
             progs_after > 0
         ):  # Can't PAMM all programmes otherwise competition ratio becomes infinite
-            ratio_after_exact = prefs_after / progs_after
-            ratio_after_2dp = round(ratio_after_exact, 2)
-            if ratio_after_2dp == ratio_after_ukfpo:
-                valid_pamm.append(pamm)
+            exact_ratio_after = prefs_after / progs_after
+            ratio_after_2dp = round(exact_ratio_after, 2)
+            if ratio_after_2dp == pub_ratio_after:
+                valid_pamm_vals.append(num_pamm)
 
     # If there's only one valid PAMM value, return it
-    if len(valid_pamm) == 1:
-        return valid_pamm[0]
+    if len(valid_pamm_vals) == 1:
+        return valid_pamm_vals[0]
 
     # Otherwise return the min and max valid PAMM values
-    pamm_bounds = (min(valid_pamm), max(valid_pamm))
+    pamm_bounds = (min(valid_pamm_vals), max(valid_pamm_vals))
     return pamm_bounds
